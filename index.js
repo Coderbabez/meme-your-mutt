8bf32a00b781fd5c02f4d77151badf0bc14aaa45
ebw8995 <emmaweeks@Emmas-MacBook-Air.local>
2017-05-11 18:40:11 -0400

    Created var's for four characteristics

---
+++ index.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	console.log("ready");
+	
+	var diva = [
+		"Communicates via hair flips",
+		"first-name basis at DryBar",
+		"gets mad when she doesnt get first row at SOUL",
+		"has 3 desk fans at work",
+		"never leaves the house without 'putting her face on'",
+		"woke up like this",
+		"Wants to speak to your manager.",
+		"Is gonna send this dish back until 'they get it right.'",
+		"Swears by gel manicures.",
+		"complains that her white wine spritzer is flat.",
+		"furious that more people aren't liking her new profile picture",
+	];
+	var active = [
+		"has different shoes for running, for weightlifting, for yoga, for basketball",
+		"does tough mudder for fun",
+		"got a fancy desk so that it could be a standing desk",
+		"runs marathons",
+		"sits on an exercise ball because its good for their back",
+		"Wakes up at 5am to jog.",
+		"Does crossfit twice a week.",
+		"Takes gym selfies (tracking those #gains.)",
+		"Always carries a Whey protein shake.",
+		"Never skips leg day.",
+		"always carries bananas for protein shakes",
+	];
+	var grumpy = [
+		"complains about the thermostat",
+		"gives you the stink eye",
+		"has big opinions about how strangers should drive and walk",
+		"hates waiting in lines",
+		"is really sarcastic to strangers on the street",
+		"is nightmare to customer service representatives",
+		"Says 'EXCUSE ME' like it's a weapon",
+		"says SORRY in a way that makes it clear that no YOU should be sorry",
+		"says THANKS sarcastically",
+		"takes personal offense to the weather",
+		"has a voice mail memo that says 'please do not leave voicemails.'",
+	];
+	var basic = [
+		"Can never decide between the Valencia or the X-Pro Instagram filter.",
+		"DEFINITELY going to Coachella this year and DEFINITELY doesn't know who's playing.",
+		"Pumpkin Spice Lattes are 'her jam.'",
+		"Has perfected the 'skinny arm' pose.",
+		"Go-to Karaoke song: Call Me Maybe.",
+		"Wears leggings and Uggs all winter.",
+		"Has every color of the Kylie lip kit.",
+		"Wears LuLu Lemon for running errands.",
+		"So #grateful for dry shampoo.",
+		"Master of the 'duck face' selfie.",
+		"Has to pee but doesn't feel like taking off her romper.",
+		"Going through a 'gigantic floppy hat' phase.",
+		"Favorite quote: 'Do you have any Rosé?'",
+		"Couldn't tell you what 'it' is but is definitely #Overit.",
+		"'Doesn't have a preference' for her bridesmaid dress but will Not wear a halter.",
+	];
+
+});
